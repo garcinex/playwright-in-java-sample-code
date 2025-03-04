84314700ce0c7c7c559aebae6cb47d4fb281b219
Marcin <garcinex@gmail.com>
2025-03-04 11:44:10 +0100

    first test finished

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -3,21 +3,21 @@ package com.serenitydojo.playwright;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ASimplePlaywrightTest {
 
     @Test
     void shouldShowThePageTitle() {
+        // TODO: Write your first playwright test here
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch();
         Page page = browser.newPage();
 
-        page.navigate("https://practicesoftwaretesting.com");
-        String title = page.title();
+        page.navigate("https://practicesoftwaretesting.com/");
 
-        Assertions.assertTrue(title.contains("Practice Software Testing"));
+        Assertions.assertThat(page.title()).contains("Practice");
 
         browser.close();
         playwright.close();
