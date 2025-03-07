8642ed87d6ee8fc9cc4562a1d60f9bdd1ddd8407
Marcin <garcinex@gmail.com>
2025-03-07 14:09:15 +0100

    browser config

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -1,25 +1,54 @@
 package com.serenitydojo.playwright;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-import org.assertj.core.api.Assertions;
+import com.microsoft.playwright.junit.UsePlaywright;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.*;
+
+@UsePlaywright
 public class ASimplePlaywrightTest {
 
-    @Test
-    void shouldShowThePageTitle() {
-        // TODO: Write your first playwright test here
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
+    private Page page;
+    private Browser browser;
+    private Playwright playwright;
+
+    @BeforeEach
+    void setUp() {
+        playwright = Playwright.create();
+        browser = playwright
+                .chromium()
+                .launch(new BrowserType.LaunchOptions()
+                        .setHeadless(false)
+                        .setArgs(Arrays.asList("--start-maximized", "--no-sandbox", "--disable-gpu")));
+        page = browser.newPage();
 
         page.navigate("https://practicesoftwaretesting.com/");
+    }
 
-        Assertions.assertThat(page.title()).contains("Practice");
-
+    @AfterEach
+    void tearDown() {
         browser.close();
         playwright.close();
+    }
+
+    @Test
+    void showThePageTitle() {
+        assertThat(page.title()).contains("Practice");
+    }
+
+    @Test
+    void searchByKeyword() {
+        page.locator("[placeholder='Search']").fill("Pliers");
+        page.locator("button:has-text('Search')").click();
+
+        assertThat(page.locator(".card").count()).isGreaterThan(0);
     }
 }
